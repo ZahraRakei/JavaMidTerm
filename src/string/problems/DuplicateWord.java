a380fd0c9479f9bd579efb0c7e243ee2f9ceed8a
Mary <zrakei.qa@gmail.com>
2021-08-27 13:35:18 -0400

    1

---
+++ src/string/problems/DuplicateWord.java
@@ -0,0 +1,29 @@
+package string.problems;
+
+public class DuplicateWord {
+
+    public static void main(String[] args) {
+
+        /*
+         Write a java program to find the duplicate words and their number of occurrences in the string.
+            Also Find the average length of the words.
+         */
+
+        String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+        String[] words = st.split(" ");
+        int count;
+        for (int i = 0; i < words.length; i++) {
+            count = 1;
+            for (int j = i + 1; j < words.length; j++) {
+                if (words[i].equals(words[j])) {
+                    count++;
+                    words[j] = "0";
+                }
+            }
+            if (count > 1 && words[i] != "0") {
+                System.out.println(words[i]);
+            }
+        }
+
+    }
+}
